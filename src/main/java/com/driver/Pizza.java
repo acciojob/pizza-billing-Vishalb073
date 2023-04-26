cc08026273a1633f318c5cc0302439f1df50424e
Vishalb073 <117665872+Vishalb073@users.noreply.github.com>
2023-04-26 13:55:12 +0530

    pizza bill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,86 +2,103 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
-    private int basePrice;
+    public int cheesePrice;
 
-    private int vegBasePrice = 300;
-    private int nonvegBaseprice = 400;
-    private int cheaseprice = 80;
-    private int extoppingsnonveg = 120;
-    private int extraToppingsVeg = 70;
-    private int bagPrice = 20;
-    private boolean ischeaseAdded;
-   private  boolean isextraTopppings;
-   private int toppingPrice;
+    public int basePrice;
+
+    public int toppingsPrice;
+
+    public int paperBagPrice;
+
+    public String bill;
+
+    public int totalPrice;
+
+    boolean isCheeseAdded;
+    // 2 benefits of these
+    //1. It prevents duplicate addition of the cheese if called multiple times
+    //2. It also tells which all things have been added
 
 
-   private boolean istakeAway;
+    boolean isToppingsAdded;
+
+    boolean isPaperBagAdded;
+
+    boolean isBillGenerated;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
-       basePrice =gateBaseprice();
-       price = basePrice;
-        ischeaseAdded = false;
+
+        //Constructor is used to
+        //initialize the attributes of any class
+
+        if(isVeg){
+            basePrice = 300;
+            toppingsPrice = 70;
+        }else{
+            //non veg case
+            basePrice = 400;
+            toppingsPrice = 120;
+        }
+        paperBagPrice = 20;
+        cheesePrice = 80;
+
+        isCheeseAdded = false;
+        isToppingsAdded = false;
+        isPaperBagAdded = false;
+
+
+        totalPrice = basePrice;
+        bill = "Base Price Of The Pizza: "+basePrice + "\n";
     }
 
     public int getPrice(){
-
-        return this.price;
-    }
-    private int gateBaseprice(){
-        return  price = isVeg ? vegBasePrice : nonvegBaseprice;
+        return totalPrice;
     }
 
     public void addExtraCheese(){
-        // your code goes here
-        if(!ischeaseAdded){
-            ischeaseAdded =true;
-            price+=cheaseprice;
+
+        if(isCheeseAdded==false){
+            totalPrice = totalPrice + cheesePrice;
+            isCheeseAdded= true; //So to prevent it from adding again
         }
+
     }
 
     public void addExtraToppings(){
-        // your code goes here
-        if (! isextraTopppings){
-            isextraTopppings = true;
-           if (isVeg) {
-               toppingPrice+=extraToppingsVeg;
-               price += extraToppingsVeg;
-           }
-           else{
-               toppingPrice+=extoppingsnonveg;
-               price+=extoppingsnonveg;
-           }
+
+        if(isToppingsAdded==false){
+            totalPrice = totalPrice + toppingsPrice;
+            isToppingsAdded = true;
         }
     }
 
     public void addTakeaway(){
-        // your code goes here
-        if(!istakeAway){
-            istakeAway = true;
-            price+=bagPrice;
+
+        if(isPaperBagAdded==false){
+            totalPrice = totalPrice + paperBagPrice;
+            isPaperBagAdded = true;
         }
     }
 
     public String getBill(){
-        // your code goes here
-        String var = "";
-        var+= "Base Price of the Pizza : " + basePrice +"\n";
-        if(ischeaseAdded){
-            var+= "Extra Cheese Added : "+cheaseprice + "\n";
+
+        if(isBillGenerated==false){
+
+
+            if(isCheeseAdded==true){
+                bill = bill + "Extra Cheese Added: "+cheesePrice + "\n";
+            }
+            if(isToppingsAdded==true){
+                bill = bill + "Extra Toppings Added: "+toppingsPrice + "\n";
+            }
+            if(isPaperBagAdded==true){
+                bill = bill + "Paperbag Added: "+paperBagPrice + "\n";
+            }
+
+            bill = bill + "Total Price: "+totalPrice + "\n";
+
+            isBillGenerated = true;
         }
-        if(isextraTopppings){
-            var+= "Extra Topping Added : " + toppingPrice + "\n";
-        }
-        if(istakeAway){
-            var+= "Extra paperbag price : " + bagPrice + "\n";
-        }
-        var += "total Price :" + price +"\n";
-        this.bill = var;
-        return this.bill;
+        return bill;
     }
 }
